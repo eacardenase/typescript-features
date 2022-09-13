4ed16158eef6193d40e096a5fe42c9f29056e7e6
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-09-13 16:34:49 -0500

    Decorators with constructor functions / classes

--- decorators.ts
+++ decorators.ts
@@ -1,3 +1,4 @@
+@classDecorator
 class Boat {
     @testDecorator
     color: string = 'red';
@@ -18,6 +19,10 @@ class Boat {
             console.log('nothing at all');
         }
     }
+}
+
+function classDecorator(constructor: typeof Boat /**Or just Function */) {
+    console.log(constructor);
 }
 
 function parameterDecorator(target: any, key: string, index: number) {
